228e01ddbf7948d569fc1ca654bacd68d74ddf34
Seth Greenstein <sg@sethgreenstein.com>
2013-10-16 09:01:40 -0400

    update website

--- jenkinstest/Controllers/HomeController.cs
+++ jenkinstest/Controllers/HomeController.cs
@@ -10,14 +10,14 @@ namespace jenkinstest.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "This is a demonstration projest to show the capabilities of Continuous Integration";
 
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+            ViewBag.Message = "Williams Forrest Continous Integration Demo";
 
             return View();
         }
